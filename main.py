6ad64b61964485a51f48b98b0047f23ce3f7de0c
alexandr2o2 <aaz75@tpu.ru>
2022-02-25 22:24:05 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Скрипт для вычисления центроиды функции
+
+import data
+def fiveChennels(data):
+    pass
+
+
+def firstMoment(data):
+    pass
+
+
+def main():
+    d = data.data
+
+    print(d)
+
+    print("Hi, my project!")
+
+
+if __name__ == '__main__':
+    main()
