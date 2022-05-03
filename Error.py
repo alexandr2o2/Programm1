7b5ff40344ef3dbf843e231e1294ff61aafe096d
alexandr2o2 <aaz75@tpu.ru>
2022-05-03 15:42:15 +0700

    *

---
+++ Error.py
@@ -0,0 +1,6 @@
+class Error(Exception):
+    """Базовый класс для других исключений"""
+    pass
+
+class DifferentInLengthOfList(Error):
+    pass
---
+++ curveFitting/fittingFunc.py
@@ -0,0 +1,77 @@
+import random
+
+import numpy as np
+import matplotlib.pyplot as plt
+import Error
+
+
+def lineAppr(x: list, y: list):
+    if len(x) != len(y):
+        return None
+    N = len(x)
+    a2 = 0
+    for i in x:
+        a2 += i * i
+    a2 = a2 / N
+    a1 = 0
+    for i in range(N):
+        a1 += x[i] * y[i]
+    a1 = a1 / N
+    a3 = 0
+    a3 = sum(x) / N
+    a4 = sum(y) / N
+    k = (a1 - a3 * a4) / (a2 - a3 * a3)
+    b = a4 - k * a3
+    x_f = x
+    y_f = [k * i + b for i in x_f]
+    return x_f, y_f
+
+
+def gauss1(x, h, c, d):
+    return h * np.exp(-(np.square((x - c) / d)) / 2)
+
+
+def gauss2(x, h1, c1, d1, h2, c2, d2):
+    return gauss1(x, h1, c1, d1) + gauss1(x, h2, c2, d2)
+
+
+def gauss3(x, h1, c1, d1, h2, c2, d2, h3, c3, d3):
+    return gauss2(x, h1, c1, d1, h2, c2, d2) + gauss1(x, h3, c3, d3)
+
+
+def gauss4(x, h1, c1, d1, h2, c2, d2, h3, c3, d3, h4, c4, d4):
+    return gauss2(x, h1, c1, d1, h2, c2, d2) + gauss2(x, h3, c3, d3, h4, c4, d4)
+
+
+def nonLineAppr(x: list, y: list):
+    pass
+
+
+def smooth1(x: list, y: list):
+    y_smooth = []
+    y_smooth.append((3 * y[0] + 2 * y[1] + y[2] - y[4]) / 5)
+    y_smooth.append((4 * y[0] + 3 * y[1] + 2 * y[2] + y[3]) / 10)
+    for i in range(2, len(y) - 2):
+        y_smooth.append((y[i - 2] + y[i - 1] + y[i] + y[i + 1] + y[i + 1]) / 5)
+    y_smooth.append((4 * y[-1] + 3 * y[-2] + 2 * y[-3] + y[-4]) / 10)
+    y_smooth.append((3 * y[-1] + 2 * y[-2] + y[-3] - y[-4]) / 5)
+    return x, y_smooth
+
+
+def main():
+    h = 10 / np.sqrt(2 * np.pi)
+    d = 100
+    c = 2500
+    x_dots = list(range(0, 4016, 1))
+    y_dots = [h * np.exp(-(np.square((x - c) / d)) / 2) for x in x_dots]
+
+    x_f, y_f = smooth1(x_dots, y_dots)
+
+    fig, ax = plt.subplots()
+    ax.plot(x_dots, y_dots)
+    ax.plot(x_f, y_f)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
